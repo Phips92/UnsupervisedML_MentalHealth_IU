2c89ebfe1bab290dc3555982c1969dfc2ee8e3fb
Philipp Mc Guire <philipp92.mcguire@gmail.com>
2025-01-04 10:14:11 +0100

    Analyze and visualize missing values, document column types
    Details:
     - Analyzed missing values in the dataset using "isnull().sum()" and
    filtered columns with missing values.
     - Printed a list of columns with missing values to provide insights
    into the scope of data quality issues.
     - Identified and counted categorical and numerical columns using
    "select_dtypes()".
     - Printed the list and count of categorical and numerical columns for
    better understanding of the dataset structure.
     - Visualized the distribution of missing alues across columns using a
    bar plot for clearer representation.
    
    Reason:
     - Understanding missing values is crucial for data preprocessing and
    cleaning.
     - Categorizing columns by type helps in planing preprocessing
    techniques.

--- Exploratory_Analysis.py
+++ Exploratory_Analysis.py
@@ -1,24 +1,52 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
 
 # Load the dataset
 file_path = "mental-heath-in-tech-2016_20161114.csv"
 data = pd.read_csv(file_path)
 
+"""
 # Extract column names with indices and save to a text file (better overview of head...)
 # dic comprehension
 column_mapping = {i: col for i, col in enumerate(data.columns)}
 with open("column_names.txt", "w") as file:
     for idx, name in column_mapping.items():
         file.write(f"{idx}: {name}\n")
+"""
 
 # Replace column names with their indices in the dataset
 data.columns = range(len(data.columns))
 
-
+"""
 # Initial data overview
 print("Column names replaced with indices")
 print("Data Overview:")
 print(data.head())
 print(data.info())
 print(data.describe(include="all"))
+"""
+
+# Analyze missing values
+missing_values = data.isnull().sum()
+missing_values = missing_values[missing_values > 0]
+print("\n\n\nColumns with Missing Values:")
+print(missing_values)
+
+# Document column types and count them
+categorical_col = data.select_dtypes(include=["object"]).columns
+numerical_col = data.select_dtypes(include=["int64", "float64"]).columns
+
+print("\n\n\nCategorical Columns:", categorical_col.tolist(), len(categorical_col.tolist()))
+print("Numerical Columns:", numerical_col.tolist(), len(numerical_col.tolist()))
+
+# Visualize missing values
+plt.figure(figsize=(16, 8))
+missing_values.sort_values(ascending=False).plot(kind="bar", color="skyblue")
+plt.title("Missing Values per Column")
+plt.xlabel("Columns")
+plt.ylabel("Count of Missing Values")
+plt.grid(axis="y", linestyle="--", alpha=0.7)
+plt.show()
