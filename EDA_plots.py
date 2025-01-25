3d9fe4b948453fa5acaf6ada1290908296c60946
Philipp Mc Guire <philipp92.mcguire@gmail.com>
2025-01-25 20:01:53 +0100

    Added Regional Distribution Visualizations for Living and Working Locations
    Details:
    - Visualized the distribution of data points for living and working countries using "57_region" and "59_region".
    - Visualized the distribution of data points for living and working US states using "58_us_region" and "60_us_region".
    - Utilized bar and pie charts to represent regional data distributions effectively.
    
    Purpose:
    - Provides insights into the geographic spread of living and working locations for both countries and US states.

--- EDA_plots.py
+++ EDA_plots.py
@@ -1,14 +1,16 @@
 import pandas as pd
+import geopandas as gpd
 from visualization import (
     plot_one_hot_encoded_distribution,
     plot_correlation_heatmap,
     plot_unknown_values,
     plot_age_distribution,
+    plot_region_distribution,
 )
 
 # Load your cleaned dataset
 data = pd.read_csv("clustered_dataset.csv")
-
+"""
 # Role Columns
 role_columns = [col for col in data.columns if col.startswith("role_")]
 
@@ -36,3 +38,40 @@ plot_age_distribution(data, "55")
 
 # Plot Unknown Values
 plot_unknown_values(data)
+"""
+"""
+shapefile_path = "/home/philipp/Desktop/Data_Science_Studium/3.Semester/Unsupervised_ML/Development/world_map/ne_110m_admin_0_countries.shp"
+# Load the shapefile
+world = gpd.read_file(shapefile_path)
+
+# Inspect the available columns
+print(world.columns)
+"""
+
+# Visualize the distribution for "57_region"
+plot_region_distribution(data, region_column="57_region", title="Data Points by Region (Living Country)")
+
+# Visualize the distribution for "59_region"
+plot_region_distribution(data, region_column="59_region", title="Data Points by Region (Working Country)")
+
+# Visualize the distribution for "57_region"
+plot_region_distribution(data, region_column="58_us_region", title="Data Points by Region (Living US State)")
+
+# Visualize the distribution for "59_region"
+plot_region_distribution(data, region_column="60_us_region", title="Data Points by Region (Working US State)")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
